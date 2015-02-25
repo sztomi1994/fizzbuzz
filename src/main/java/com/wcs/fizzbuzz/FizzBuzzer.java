5f37bbde2bd96067c552687b8bc6c37968f0f4e2
Nagy-Kovács Máté Norman <nkm.norman.89@gmail.com>
2015-02-25 18:05:48 +0100

    sanyi

--- src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
+++ src/main/java/com/wcs/fizzbuzz/FizzBuzzer.java
@@ -1,9 +1,24 @@
 package com.wcs.fizzbuzz;
 
 public class FizzBuzzer {
-    
+
     public String execute(int number) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String message = "";
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (number % 3 == 0) {
+
+            message += "fizz";
+        }
+        if (number % 5 == 0) {
+
+            message += "buzz";
+        }
+     
+        if (message.isEmpty()) {
+            message+=number;
+        }
+        return message;
     }
-    
+
 }
